1cf2b7de9cdc9ee3fa83d206b7e429019be0ecfe
farazazeem-6 <faraz.tacticminds.com>
2025-10-09 15:05:15 +0500

    episode-4

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
-const App = () => {
-   return <h1 style={{color:'black'}}>Hello World</h1>
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg" alt="" />
+            </div>
+            <div className="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="res-container">
+            <Card />
+        </div>
+    )
+
+}
+
+const Card = () => {
+    return (
+        <div className="res-card">
+            <img src={''} alt={''} />
+            <h6></h6>
+            <h6></h6>
+            <h6></h6>
+        </div>
+    )
+
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(<AppLayout />)
