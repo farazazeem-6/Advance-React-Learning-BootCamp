5f28ffebac0711e3e8adb75119d463858ee76999
farazazeem-6 <faraz.tacticminds.com>
2025-10-17 16:13:02 +0500

    changes

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const Body = () => {
+  return (
+    <div className="res-container">
+      <Card />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="res-card">
+      {/* <img src={''} alt={''} /> */}
+      <h6>Name</h6>
+      <h6>Id</h6>
+      <h6>Detail</h6>
+    </div>
+  );
+};
+export default Card;
